e78498bc863683d6fcf65d9dd4f5747ba5b80a28
jonathan.ferreira <jonathan.ferreira@animaeducacao.com.br>
2017-06-05 17:37:49 -0300

    Basic Structure

---
+++ vue-webpack-complete-template/src/components/home/hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+    <example2></example2>
+  </div>
+</template>
+
+<script>
+
+import Example2 from '../shared/example2/example2.vue'
+
+export default {
+  name: 'hello',
+  components: {
+    Example2
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
